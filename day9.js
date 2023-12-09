6cb796a3b519d8f44724fcbdf641c94e09c0bf7d
Yeunha Kim <yeunhakim93@gmail.com>
2023-12-08 21:29:12 -0800

    Day 9 - raw

---
+++ day9.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const input = fs.readFileSync("inputs/day9_input.txt", "utf8");
+
+// (function solution(input) {
+//   const lines = input.split("\n");
+//   let result = 0;
+
+//   lines.forEach((line) => {
+//     const sequence = [line.split(" ").map((num) => parseInt(num))];
+
+//     let i = 0;
+
+//     while (true) {
+//       sequence.push(getNextSequence(sequence[i++]));
+//       if (/^0*$/.test(sequence[i].join(""))) break;
+//     }
+//     for (let j = sequence.length - 1; j > 0; j--) {
+//       let currLen = sequence[j].length - 1;
+//       let prevLen = sequence[j - 1].length - 1;
+//       sequence[j - 1].push(sequence[j - 1][prevLen] + sequence[j][currLen]);
+//     }
+//     console.log(sequence[0][sequence[0].length - 1]);
+//     result += sequence[0][sequence[0].length - 1];
+//   });
+//   console.log("====");
+//   console.log(result);
+//   return result;
+// })(input);
+
+(function solution2(input) {
+  const lines = input.split("\n");
+  let result = 0;
+
+  lines.forEach((line) => {
+    const sequence = [line.split(" ").map((num) => parseInt(num))];
+
+    let i = 0;
+
+    while (true) {
+      sequence.push(getNextSequence(sequence[i++]));
+      if (/^0*$/.test(sequence[i].join(""))) break;
+    }
+    sequence[sequence.length - 1].unshift(0);
+    for (let j = sequence.length - 1; j > 0; j--) {
+      const val = sequence[j - 1][0] - sequence[j][0];
+      sequence[j - 1].unshift(val);
+    }
+  });
+  console.log("====");
+  console.log(result);
+  return result;
+})(input);
+
+function getNextSequence(sequence) {
+  const result = [];
+  for (let i = 1; i < sequence.length; i++) {
+    result.push(sequence[i] - sequence[i - 1]);
+  }
+  return result;
+}
