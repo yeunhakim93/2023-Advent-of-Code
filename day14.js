39be76ca9d3420a0bc273572c2e3a67bd5906145
Yeunha Kim <24959947+yeunhakim93@users.noreply.github.com>
2023-12-13 21:28:41 -0800

    Day 14 Part 1 - raw

---
+++ day14.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync("inputs/day14_input.txt", "utf8");
+
+(function solution1(input) {
+  const lines = input.split("\n");
+
+  let result = 0;
+
+  const map = lines.map((line) => line.split(""));
+
+  console.table(map);
+
+  for (let j = 0; j < map[0].length; j++) {
+    let offset = 0;
+    let pointer = 0;
+    let currChar = map[pointer][j];
+    while (pointer < map.length) {
+      currChar = map[pointer][j];
+      if (currChar === ".") {
+      } else if (currChar === "O") {
+        result += map.length - offset;
+        offset++;
+      } else if (currChar === "#") {
+        offset = pointer + 1;
+      }
+      pointer++;
+    }
+  }
+
+  console.log("=====");
+  console.log(result);
+  return result;
+})(input);
