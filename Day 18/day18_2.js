b7ab8b9ffeda3c164a7eda83ce5b32850e69b5c6
Yeunha Kim <24959947+yeunhakim93@users.noreply.github.com>
2023-12-22 12:22:08 -0800

    Day 18 - pt 2 Co-authored-by: Justin Bird <justin@thewordisbird.dev>

--- Day 18/day18_2.js
+++ Day 18/day18_2.js
@@ -1,37 +1,52 @@
 const fs = require("fs");
 const input = fs.readFileSync("inputs/day18_input.txt", "utf8");
 
-(function solution1(input) {
+(function solution2(input) {
   let result = 0;
-  const lines = input.split("\n");
+  let area = 0;
+  let perimeter = 0;
 
-  const position = [0, 0];
+  const lines = input.split("\n");
+  const position = [1, 1];
+
   lines.forEach((line) => {
-    const [direction, steps, _] = line.split(" ");
-    const distance = parseInt(steps);
+    const [_, __, color] = line.split(" ");
+    const hexNum = color.slice(2, 7);
+    const direction = color[7];
+    const distance = parseInt(hexNum, 16);
+
     let currPosition;
     switch (direction) {
-      case "U":
-        currPosition = [position[0], position[1] + distance];
-        break;
-      case "D":
-        currPosition = [position[0], position[1] - distance];
-        break;
-      case "L":
-        currPosition = [position[0] - distance, position[1]];
-        break;
-      case "R":
+      case "0":
         currPosition = [position[0] + distance, position[1]];
         break;
+      case "1":
+        currPosition = [position[0], position[1] - distance];
+        break;
+      case "2":
+        currPosition = [position[0] - distance, position[1]];
+        break;
+      case "3":
+        currPosition = [position[0], position[1] + distance];
+        break;
     }
-    let area = position[0] * currPosition[1] - position[1] * currPosition[0];
-    console.log(position, currPosition, area);
-    result += area;
+    let currArea =
+      position[0] * currPosition[1] - position[1] * currPosition[0];
+    perimeter += distance;
+    area += currArea / 2;
     position[0] = currPosition[0];
     position[1] = currPosition[1];
   });
 
+  let exteriorPts = (lines.length + 4) / 2;
+  let interiorPts = lines.length - exteriorPts;
+
+  result += Math.abs(area);
+  result += (perimeter - exteriorPts - interiorPts) / 2;
+  result += (exteriorPts * 3) / 4;
+  result += interiorPts / 4;
+
   console.log("=====");
-  console.log(Math.abs(result) / 2);
+  console.log(result);
   return result;
 })(input);
